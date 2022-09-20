087eb471432444cd429bdf8d6acb702ff043f41a
Patrick Brady <patrick.brady@daveramsey.com>
2022-09-20 16:50:42 -0500

    First implementation

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,10 @@
-describe("with two numbers to multiply", () => {
-  it("returns the result", () => {
+import { Dollar } from '..';
+
+describe('with two numbers to multiply', () => {
+  it('returns the result', () => {
     const five = new Dollar(5);
     five.times(2);
 
     expect(five.amount).toEqual(10);
-  })
-})
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+export class Dollar {
+  public amount: number;
 
+  constructor(amount: number) {
+    this.amount = 10;
+  }
+
+  public times(value: number) {
+    return value;
+  }
+}
